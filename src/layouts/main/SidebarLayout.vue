9d9203ee738982aab413d3979adc6295335d77ff
Andrey Smirnov <muenchen.webas@gmail.com>
2020-11-01 01:36:04 +0100

    Начало автогенерации меню из данных.

--- src/layouts/main/SidebarLayout.vue
+++ src/layouts/main/SidebarLayout.vue
@@ -1,4 +1,72 @@
 <template>
+<div id="temp">
+  <b-nav class="navbar-nav bg-gradient-primary sidebar sidebar-dark">
+
+    <!-- Sidebar - Brand -->
+    <b-navbar-brand href="#" class="sidebar-brand d-flex align-items-center justify-content-center">
+      <b-icon icon="emoji-laughing" rotate="-15" class="sidebar-brand-icon"></b-icon>
+      <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+    </b-navbar-brand>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0" />
+
+    <div
+      v-for="(menuElementsGroup, groupId) in menuElementsGroups"
+      :key="groupId"
+      class="nav-item"
+    >
+      <!-- Heading -->
+      <div v-if="menuElementsGroup.name" class="sidebar-heading">{{ menuElementsGroup.name }}</div>
+
+      <div
+        v-for="(element, elementId) in menuElementsGroup.elemets"
+        :key="elementId"
+      >
+        <!-- Nav Item - Dashboard -->
+        <b-nav-item
+          active-class="active"
+          class="my-0"
+          v-b-toggle="(element.childGroups) ? element.name : ''"
+          :to="(element.url != '') ? element.url : '#'"
+          :disabled="element.url == '#'"
+        >
+          <b-icon :icon="element.icon"></b-icon>
+          <span>{{element.name}}</span>
+        </b-nav-item>
+
+        <b-collapse
+          v-if="element.childGroups"
+          :id="element.name"
+          class="collapse show"
+        >
+          <div class="bg-white py-2 collapse-inner rounded">
+            <div
+              v-for="(childGroup, childGroupId) in element.childGroups"
+              :key="childGroupId"
+            >
+              <h6 class="collapse-header">{{childGroup.name}}:</h6>
+              <router-link
+                v-for="(child, childId) in childGroup.childs"
+                :key="childId"
+                class="collapse-item"
+                :to="(child.url != '') ? child.url : '#'"
+                :disabled="child.url == '#'"
+                active-class="active"
+              >
+                {{child.name}}
+              </router-link>
+            </div>
+          </div>
+        </b-collapse>
+
+      </div>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider" />
+    </div>
+  </b-nav>
+
   <ul
     class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
     id="accordionSidebar"
@@ -154,4 +222,125 @@
       </button>
     </div>
   </ul>
+</div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    menuElementsGroups: [
+      {
+        elemets: [
+          {
+            name: 'Dashboard',
+            icon: 'gem',
+            url: '#'
+          }
+        ]
+      },
+      {
+        name: 'Interface',
+        elemets: [
+          {
+            name: 'Components',
+            icon: 'gear-fill',
+            childGroups: [
+              {
+                name: 'custom components',
+                childs: [
+                  {
+                    name: 'Buttons',
+                    url: '#'
+                  },
+                  {
+                    name: 'Cards',
+                    url: '#'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'Utilities',
+            icon: 'tools',
+            childGroups: [
+              {
+                name: 'custom Utilities',
+                childs: [
+                  {
+                    name: 'Colors',
+                    url: '#'
+                  },
+                  {
+                    name: 'Borders',
+                    url: '#'
+                  },
+                  {
+                    name: 'Animations',
+                    url: '#'
+                  },
+                  {
+                    name: 'Other',
+                    url: '#'
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Addons',
+        elemets: [
+          {
+            name: 'Pages',
+            icon: 'folder-fill',
+            childGroups: [
+              {
+                name: 'login screens:',
+                childs: [
+                  {
+                    name: 'Login',
+                    url: '#'
+                  },
+                  {
+                    name: 'Register',
+                    url: '#'
+                  },
+                  {
+                    name: 'Forgot Password',
+                    url: '#'
+                  }
+                ]
+              },
+              {
+                name: 'login screens:',
+                childs: [
+                  {
+                    name: '404 Page',
+                    url: '#'
+                  },
+                  {
+                    name: 'Blank Page',
+                    url: '/blank'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'Charts',
+            icon: 'bar-chart-line-fill',
+            url: '#'
+          },
+          {
+            name: 'Tables',
+            icon: 'table',
+            url: '#'
+          }
+        ]
+      }
+    ]
+  })
+}
+</script>
