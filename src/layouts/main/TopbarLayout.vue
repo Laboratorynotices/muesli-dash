1e5acafab4c0b19743b1ea0f673a42620f4bbdb0
Andrey Smirnov <muenchen.webas@gmail.com>
2021-01-15 02:20:28 +0100

    Подправил изображение поиска в шапке сайта.

--- src/layouts/main/TopbarLayout.vue
+++ src/layouts/main/TopbarLayout.vue
@@ -7,28 +7,11 @@
       id="sidebarToggleTop"
       class="btn btn-link d-md-none rounded-circle mr-3"
     >
-      <i class="fa fa-bars"></i>
+      <b-icon-list />
     </button>
 
     <!-- Topbar Search -->
-    <form
-      class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
-    >
-      <div class="input-group">
-        <input
-          type="text"
-          class="form-control bg-light border-0 small"
-          placeholder="Search for..."
-          aria-label="Search"
-          aria-describedby="basic-addon2"
-        />
-        <div class="input-group-append">
-          <button class="btn btn-primary" type="button">
-            <i class="fas fa-search fa-sm"></i>
-          </button>
-        </div>
-      </div>
-    </form>
+    <topbarSearch />
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
@@ -280,3 +263,14 @@
     </ul>
   </nav>
 </template>
+
+<script>
+import TopbarSearch from '@/layouts/main/topbar/Search.vue'
+
+export default {
+  name: 'Topbar',
+  components: {
+    TopbarSearch
+  }
+}
+</script>
---
+++ src/layouts/main/topbar/Search.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-nav-form
+      form-class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
+      @submit.stop.prevent="alert('Form Submitted')"
+    >
+      <b-input-group>
+        <b-form-input
+          class="form-control bg-light border-0 small"
+          placeholder="Search for..."
+          aria-label="Search"
+          aria-describedby="basic-addon2"
+        ></b-form-input>
+        <b-input-group-append>
+          <b-button
+            variant="primary"
+            type="submit">
+            <b-icon-search scale="0.66"  />
+          </b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-nav-form>
+</template>
